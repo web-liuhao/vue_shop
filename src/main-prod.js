a4ce6bf5ad84d810842b8c007f66135fa01fffcb
web-liuhao <liuhao199727@gmail.com>
2020-05-06 13:45:09 +0800

    进行了一些webpack配置

---
+++ src/main-prod.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+// import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+// 导入字体样式
+import './assets/fonts/iconfont.css'
+import TreeTable from 'vue-table-with-tree-grid'
+
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+
+// 导入 NProgress 对应的js和css
+import NProgress from 'nprogress'
+
+import axios from 'axios'
+// 配置请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在 request 拦截器中，展示进度条 NProgress.start()
+// 配置拦截器
+axios.interceptors.request.use(config => {
+  NProgress.start()
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
+// 在 response 拦截器中，隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
+Vue.prototype.$http = axios
+
+Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
+// 将富文本编辑器，注册为全局可以的组件
+Vue.use(VueQuillEditor)
+
+// 自定义格式化时间的全局过滤器
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDay() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
